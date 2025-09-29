a3e15952d3c50bdfb4cc92e59ad1af2d55f911e0
MarvsConcept <marvade8@gmail.com>
2025-09-29 05:23:15 -0800

    Created DTOs for all Entities

---
+++ src/main/java/com/example/HotelBooking/dtos/PaymentDTO.java
@@ -0,0 +1,43 @@
+package com.example.HotelBooking.dtos;
+
+import com.example.HotelBooking.entities.User;
+import com.example.HotelBooking.enums.PaymentGateway;
+import com.example.HotelBooking.enums.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentDTO {
+
+    private Long id;
+    private BookingDTO bookingDTo;
+
+    private String transactionId;
+
+    private BigDecimal amount;
+
+    private PaymentGateway paymentMethod; // e.g. Paypal, Stripe, Flutterwave, Paystack
+
+    private LocalDateTime paymentDate;
+
+    private PaymentStatus status; // failed e.t.c
+
+    private String bookingReference;
+    private String failureReason;
+
+    private String approvalLink; //paypay payment approval URL
+
+}
