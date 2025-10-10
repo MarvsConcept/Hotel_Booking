235901361a54d1c9cc9d35e7e95d618766cf8be7
MarvsConcept <marvade8@gmail.com>
2025-10-10 13:42:41 -0800

    Update Room Controller

--- src/main/java/com/example/HotelBooking/controllers/RoomController.java
+++ src/main/java/com/example/HotelBooking/controllers/RoomController.java
@@ -7,10 +7,7 @@ import com.example.HotelBooking.services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
@@ -44,6 +41,28 @@ public class RoomController {
 }
 
 
+    @PutMapping("/update")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> updateRoom(
+            @RequestParam (value = "roomNumber", required = false) Integer roomNumber,
+            @RequestParam (value = "type", required = false) RoomType type,
+            @RequestParam (value = "pricePerNight", required = false) BigDecimal pricePerNight,
+            @RequestParam (value = "capacity", required = false) Integer capacity,
+            @RequestParam (value = "description", required = false) String description,
+            @RequestParam (value = "imageFile", required = false) MultipartFile imageFile,
+            @RequestParam (value = "id", required = true) Long id
+    ) {
+        RoomDTO roomDTO = RoomDTO.builder()
+                .id(id)
+                .roomNumber(roomNumber)
+                .type(type)
+                .pricePerNight(pricePerNight)
+                .capacity(capacity)
+                .description(description)
+                .build();
+
+        return ResponseEntity.ok(roomService.updateRoom(roomDTO, imageFile));
+    }
 
 
 
