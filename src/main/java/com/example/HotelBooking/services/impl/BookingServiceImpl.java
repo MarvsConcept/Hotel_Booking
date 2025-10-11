ffcc5ac134614db8c0a8a16dff4f3e6c3297f015
MarvsConcept <marvade8@gmail.com>
2025-10-11 15:32:28 -0800

    Implementing Booking Service - Method to Create Booking

--- src/main/java/com/example/HotelBooking/services/impl/BookingServiceImpl.java
+++ src/main/java/com/example/HotelBooking/services/impl/BookingServiceImpl.java
@@ -1,9 +1,18 @@
 package com.example.HotelBooking.services.impl;
 
 import com.example.HotelBooking.dtos.BookingDTO;
+import com.example.HotelBooking.dtos.NotificationDTO;
 import com.example.HotelBooking.dtos.Response;
+import com.example.HotelBooking.entities.Booking;
+import com.example.HotelBooking.entities.Room;
+import com.example.HotelBooking.entities.User;
+import com.example.HotelBooking.enums.BookingStatus;
+import com.example.HotelBooking.enums.PaymentStatus;
+import com.example.HotelBooking.exceptions.InvalidBookingStateAndDateException;
+import com.example.HotelBooking.exceptions.NotFoundException;
 import com.example.HotelBooking.repositories.BookingRepository;
 import com.example.HotelBooking.repositories.RoomRepository;
+import com.example.HotelBooking.services.BookingCodeGenerator;
 import com.example.HotelBooking.services.BookingService;
 import com.example.HotelBooking.services.NotificationService;
 import com.example.HotelBooking.services.UserService;
@@ -11,6 +20,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @Slf4j
@@ -23,13 +37,74 @@ public class BookingServiceImpl implements BookingService {
     private final NotificationService notificationService;
     private final ModelMapper modelMapper;
     private final UserService userService;
-
-
+    private final BookingCodeGenerator bookingCodeGenerator;
 
 
     @Override
     public Response createBooking(BookingDTO bookingDTO) {
-        return null;
+        User currentUser = userService.getCurrentLoggedInUser();
+
+        Room room = roomRepository.findById(bookingDTO.getRoomId())
+                .orElseThrow(()-> new NotFoundException("Room not found!"));
+
+        //validate : ensure the checkin date is not before today
+        if (bookingDTO.getCheckInDate().isBefore(LocalDate.now())) {
+            throw new InvalidBookingStateAndDateException("Check in date cannot be before today");
+        }
+
+        //validation: Ensure the checkout date is not before check in date
+        if (bookingDTO.getCheckOutDate().isBefore(bookingDTO.getCheckInDate())) {
+            throw new InvalidBookingStateAndDateException("Check out date cannot be before check in date");
+        }
+        //validation: Ensure the check in date is not same as check out date
+        if (bookingDTO.getCheckInDate().isEqual(bookingDTO.getCheckOutDate())) {
+            throw new InvalidBookingStateAndDateException("Check in date cannot be equal to check out date");
+        }
+
+        //Validate room availability
+        boolean isAvailable = bookingRepository.isRoomAvailable(room.getId(), bookingDTO.getCheckInDate(), bookingDTO.getCheckOutDate());
+        if (!isAvailable) {
+            throw new InvalidBookingStateAndDateException("Room is not available for the selected date ranges");
+        }
+        //calculate the total price needed to pay for a stay
+        BigDecimal totalPrice = calculateTotalPrice(room, bookingDTO);
+        String bookingReference = bookingCodeGenerator.generateBookingReference();
+
+        //create and save the booking
+        Booking booking = new Booking();
+        booking.setUser(currentUser);
+        booking.setRoom(room);
+        booking.setCheckInDate(bookingDTO.getCheckInDate());
+        booking.setCheckOutDate(bookingDTO.getCheckOutDate());
+        booking.setTotalPrice(totalPrice);
+        booking.setBookingReference(bookingReference);
+        booking.setBookingStatus(BookingStatus.BOOKED);
+        booking.setPaymentStatus(PaymentStatus.PENDING);
+        booking.setCreatedAt(LocalDateTime.now());
+
+        bookingRepository.save(booking); //save to database
+
+        //generate the payment url which will be sent via mail
+        String paymentUrl = "http://localhost:3000/payment/" + bookingReference + "/" + totalPrice;
+
+        log.info("PAYMENT LINK: {}", paymentUrl);
+
+        //send notification vie email
+        NotificationDTO notificationDTO = NotificationDTO.builder()
+                .recipient(currentUser.getEmail())
+                .subject("Booking Confirmation")
+                .body(String.format("Your booking has been created successfully, Please proceed with your payment using the payment link below " +
+                        "\n\n%s", paymentUrl))
+                .bookingReference(bookingReference)
+                .build();
+        notificationService.sendEmail(notificationDTO); //sending email
+
+        return Response.builder()
+                .status(200)
+                .message("Booking is successfully")
+                .booking(bookingDTO)
+                .build();
+
     }
 
     @Override
@@ -40,5 +115,11 @@ public class BookingServiceImpl implements BookingService {
     @Override
     public Response updateBooking(BookingDTO bookingDTO) {
         return null;
+    }
+
+    private BigDecimal calculateTotalPrice(Room room, BookingDTO bookingDTO) {
+        BigDecimal pricePerNight = room.getPricePerNight();
+        long days = ChronoUnit.DAYS.between(bookingDTO.getCheckInDate(), bookingDTO.getCheckOutDate());
+        return pricePerNight.multiply(BigDecimal.valueOf(days));
     }
 }
