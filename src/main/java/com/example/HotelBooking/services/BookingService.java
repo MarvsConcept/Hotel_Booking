100e4d7328dc87504173eb3b0ba286f6c77d4049
MarvsConcept <marvade8@gmail.com>
2025-10-11 08:30:11 -0800

    Implementing Booking Service - Created the Booking Service class then a BookingCodeGenerator Class to generate Booking References

---
+++ src/main/java/com/example/HotelBooking/services/BookingService.java
@@ -0,0 +1,15 @@
+package com.example.HotelBooking.services;
+
+import com.example.HotelBooking.dtos.BookingDTO;
+import com.example.HotelBooking.dtos.Response;
+
+public interface BookingService {
+
+    Response createBooking(BookingDTO bookingDTO);
+
+    Response findBookingByReferenceNo(String bookingReference);
+
+    Response updateBooking(BookingDTO bookingDTO);
+
+}
+
---
+++ src/main/java/com/example/HotelBooking/services/impl/BookingServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.HotelBooking.services.impl;
+
+import com.example.HotelBooking.dtos.BookingDTO;
+import com.example.HotelBooking.dtos.Response;
+import com.example.HotelBooking.repositories.BookingRepository;
+import com.example.HotelBooking.repositories.RoomRepository;
+import com.example.HotelBooking.services.BookingService;
+import com.example.HotelBooking.services.NotificationService;
+import com.example.HotelBooking.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BookingServiceImpl implements BookingService {
+
+
+    private final BookingRepository bookingRepository;
+    private final RoomRepository roomRepository;
+    private final NotificationService notificationService;
+    private final ModelMapper modelMapper;
+    private final UserService userService;
+
+
+
+
+    @Override
+    public Response createBooking(BookingDTO bookingDTO) {
+        return null;
+    }
+
+    @Override
+    public Response findBookingByReferenceNo(String bookingReference) {
+        return null;
+    }
+
+    @Override
+    public Response updateBooking(BookingDTO bookingDTO) {
+        return null;
+    }
+}
